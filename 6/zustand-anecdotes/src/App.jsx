4665ae1712c1b211ef0523968508d4b68681dbed
jrimpila <jrimpila@student.hive.fi>
2026-05-03 18:35:15 +0300

    added some stuff

---
+++ 6/zustand-anecdotes/src/App.jsx
@@ -0,0 +1,36 @@
+
+import { useAnecdotes, useAnecdoteActions } from './store'
+
+
+const App = () => {
+
+
+  const anecdotes = useAnecdotes()
+  const { increaseVotes } = useAnecdoteActions()
+
+
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => increaseVotes(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
