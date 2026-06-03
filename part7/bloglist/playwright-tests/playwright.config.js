a9eeb43eb3ef1efc53e2463789e8c902a8119148
Johannes <you@example.com>
2026-06-03 11:36:46 +0300

    started the final assignment

---
+++ part7/bloglist/playwright-tests/playwright.config.js
@@ -0,0 +1,34 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  // 🔥 FORCE SINGLE WORKER (debug mode / stable runs)
+  fullyParallel: false,
+  workers: 1,
+
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+
+  reporter: 'html',
+
+  use: {
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
