40b6aabfd9728946175bca93302c2c381c49ed57
Johannes <you@example.com>
2026-06-13 17:54:44 +0300

    refactored folder structure

--- part2/2b-phonebook/src/components/Persons.jsx
+++
@@ -1,4 +0,0 @@
-export const Persons = ({ person, filterWord }) => {
-	if (!person.name.toLowerCase().includes(filterWord.toLowerCase())) return null;
-	return <p>{person.name} {person.number}</p>;
-  };
