c118a18c9075ab28681ae36b3e010d6a2665e792
Tonttu84 <tonttu.torvinen22@gmail.com>
2026-06-23 13:10:05 +0300

    Added the bmi calculator

---
+++ part9/healthapp/exerciseCalculator.ts
@@ -0,0 +1,17 @@
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises  = (input: Array<Number>): Result => {
+
+	let value: Result;
+	
+	return value
+}
